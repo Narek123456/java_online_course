fb3867f35a9e5fb227bf9821d3b27fa6e88fea69
Narek123456 <margaryannar9898@gmail.com>
2023-01-18 20:14:53 +0400

    my first git commit

---
+++ Lesson1/src/Lesson1.java
@@ -0,0 +1,42 @@
+public class Lesson1 {
+    public static void main(String[] args) {
+        System.out.println("barev");
+        System.out.println("Lesson 1");
+
+        byte x=127;
+        System.out.println(x);
+        short y=-4542;
+        System.out.println(y);
+        int a=454564;
+        System.out.println(a);
+        long b=4564674324684L;
+        System.out.println(b);
+
+        //kamayakan amobxjakan tivy javayum inte
+        //kamayakan  racional tivy javayum doublee
+        float aa=652.6f;
+        System.out.println(aa);
+        double bb=35.8;
+        System.out.println(bb);
+        boolean v=true;
+        System.out.println(v);
+        v=false;
+        System.out.println(v);
+        char t='$';
+        System.out.println(t);
+        char tt='\u003f';
+        System.out.println(tt);
+        //sranq primitiv tipern en
+        //strinq arden primitiv che, u ed patcharov el mecatar e
+        String s="barev lesson1";
+        System.out.println(s);
+        String firstName="Narek";
+        String lastName="Margaryan";
+        System.out.println(firstName+" "+lastName);
+        int k=95;
+        System.out.println("k="+k);
+
+
+
+    }
+}
